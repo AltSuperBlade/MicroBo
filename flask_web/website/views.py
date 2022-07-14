0106d336d8e28680cd19c6018753014fc274117f
AltSuperBlade <yangyuchen2@yeah.net>
2022-07-14 18:00:44 +0800

    home页面优化+profile增加内容+部分edit页面

--- flask_web/website/views.py
+++ flask_web/website/views.py
@@ -35,18 +35,36 @@ def home():
 @views.route('/profile/<nickname>',methods=['GET', 'POST'])
 @login_required
 def profile(nickname):
+    if request.method=='POST':
+        return render_template("edit.html",user=current_user)
     user_id = User.query.filter_by(nickname=nickname).first().id
     accessingUser=User.query.get(user_id)
     return render_template("profile.html",user=current_user,accessingUser=accessingUser)
-# @views.route('/profile', methods=['GET'])
-# @login_required
-# def profile():
-#     return render_template("profile.html",user=current_user,locationid=current_user.id)
 
-# @views.route('/profile', methods=['POST'])
-# @login_required
-# def accessprofile():
-#     return render_template("profile.html",user=current_user,locationid=request.form['noteUserId'])
+
+@views.route('/edit',methods=['GET', 'POST'])
+@login_required
+def edit():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        nickname = request.form.get('nickname')
+
+        if user:
+            flash('Email already exists.', category='error')
+        elif not re.match(r'^[0-9a-zA-Z_]{0,19}@[0-9a-zA-Z]{1,13}\.[com,cn,net]{1,3}$',email):
+            flash('Email must be valid.', category='error')
+        elif len(nickname) < 2:
+            flash('Nickname must be greater than 1 character.', category='error')
+        else:
+            # new_user = User(email=email, nickname=nickname, password=generate_password_hash(
+            #     password1, method='sha256'))
+            # db.session.add(new_user)
+            # db.session.commit()
+            # login_user(new_user, remember=True)
+            flash('Account created!', category='success')
+            return redirect(url_for('views.profile/<nickname>'))
+    return render_template("edit.html",user=current_user)
+
 
 @views.route('/delete-note', methods=['POST'])
 def delete_note():
@@ -60,11 +78,3 @@ def delete_note():
 
     return jsonify({})
 
-# @views.route('/go-profile', methods=['GET','POST'])
-# def go_profile():
-#     # note = json.loads(request.data)
-#     # noteId = note['noteId']
-#     # note = Note.query.get(noteId)
-#     return render_template("profile.html",user=current_user,locationid=note.user_id)
-
-#    return jsonify({})
