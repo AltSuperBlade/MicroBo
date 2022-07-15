f0a4b5ca299f75376855618587c9974a6866af1b
AltSuperBlade <yangyuchen2@yeah.net>
2022-07-15 16:33:41 +0800

    上传图片的轮子+update README.md

--- flask_web/website/views.py
+++ flask_web/website/views.py
@@ -1,13 +1,50 @@
-from flask import Blueprint, render_template, request, flash, jsonify, redirect, current_app,url_for
+from flask import Blueprint, render_template, request, flash, jsonify, redirect, current_app,url_for,make_response
 from flask_login import login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.utils import secure_filename
+from datetime import timedelta
 from .models import Note,User
 from .forms import editEmail,editNickname,changePassword
 from . import db
 import json
+import cv2
 import re
+import os
+
 
 views = Blueprint('views', __name__)
+
+# #设置允许的文件格式
+# ALLOWED_EXTENSIONS = set(['png', 'jpg', 'JPG', 'PNG', 'bmp'])
+ 
+# def allowed_file(filename):
+#     return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+ 
+# # 设置静态文件缓存过期时间
+# app.send_file_max_age_default = timedelta(seconds=1)
+ 
+# @views.route('/upload', methods=['POST', 'GET'])  # 添加路由
+# def upload():
+#     if request.method == 'POST':
+#         f = request.files['file']
+ 
+#         if not (f and allowed_file(f.filename)):
+#             return jsonify({"error": 1001, "msg": "请检查上传的图片类型，仅限于png、PNG、jpg、JPG、bmp"})
+ 
+#         user_input = request.form.get("name")
+ 
+#         basepath = os.path.dirname(__file__)  # 当前文件所在路径
+ 
+#         upload_path = os.path.join(basepath, 'static/images',secure_filename(f.filename))  #注意：没有的文件夹一定要先创建，不然会提示没有该路径
+#         f.save(upload_path)
+ 
+#         image_data = open(upload_path, "rb").read()
+#         response = make_response(image_data)
+#         response.headers['Content-Type'] = 'image/png'
+#         return response
+ 
+#     return render_template('upload.html')
+
 
 @views.route('/favicon.ico')
 def get_fav():
