88e8388a73a2eeb57b3cbdef562a75d84db7f17b
AltSuperBlade <yangyuchen2@yeah.net>
2022-07-25 11:43:14 +0800

    部分头像内容
    电脑总重启，紧急上传

--- flask_web/website/views.py
+++ flask_web/website/views.py
@@ -14,36 +14,37 @@ import os
 
 views = Blueprint('views', __name__)
 
-# #设置允许的文件格式
-# ALLOWED_EXTENSIONS = set(['png', 'jpg', 'JPG', 'PNG', 'bmp'])
+#设置允许的文件格式
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'JPG', 'PNG', 'bmp'])
  
-# def allowed_file(filename):
-#     return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
  
-# # 设置静态文件缓存过期时间
-# app.send_file_max_age_default = timedelta(seconds=1)
+# 设置静态文件缓存过期时间
+views.send_file_max_age_default = timedelta(seconds=1)
  
-# @views.route('/upload', methods=['POST', 'GET'])  # 添加路由
-# def upload():
-#     if request.method == 'POST':
-#         f = request.files['file']
+@views.route('/upload', methods=['POST', 'GET'])  # 添加路由
+def upload():
+    if request.method == 'POST':
+        f = request.files['file']
  
-#         if not (f and allowed_file(f.filename)):
-#             return jsonify({"error": 1001, "msg": "请检查上传的图片类型，仅限于png、PNG、jpg、JPG、bmp"})
+        if not (f and allowed_file(f.filename)):
+            flash('Please check your format, we only accept png、PNG、jpg、JPG、bmp', category='error')
+            # return jsonify({"error": 1001, "msg": "请检查上传的图片类型，仅限于png、PNG、jpg、JPG、bmp"})
  
-#         user_input = request.form.get("name")
+        # user_input = request.form.get("name")
  
-#         basepath = os.path.dirname(__file__)  # 当前文件所在路径
+        basepath = os.path.dirname(__file__)  # 当前文件所在路径
  
-#         upload_path = os.path.join(basepath, 'static/images',secure_filename(f.filename))  #注意：没有的文件夹一定要先创建，不然会提示没有该路径
-#         f.save(upload_path)
+        upload_path = os.path.join(basepath, 'static/images',secure_filename(f.filename))  #注意：没有的文件夹一定要先创建，不然会提示没有该路径
+        f.save(upload_path)
  
-#         image_data = open(upload_path, "rb").read()
-#         response = make_response(image_data)
-#         response.headers['Content-Type'] = 'image/png'
-#         return response
+        # image_data = open(upload_path, "rb").read()
+        # response = make_response(image_data)
+        # response.headers['Content-Type'] = 'image/png'
+        # return response
  
-#     return render_template('upload.html')
+    return render_template('upload.html')
 
 
 @views.route('/favicon.ico')
