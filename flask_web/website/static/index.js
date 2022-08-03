d80f266b525419e6b1586c71e7d204dfa73a5363
AltSuperBlade <yangyuchen2@yeah.net>
2022-08-03 10:32:29 +0800

    一些改动

--- flask_web/website/static/index.js
+++ flask_web/website/static/index.js
@@ -3,7 +3,13 @@ function deleteNote(noteId) {
     method: "POST",
     body: JSON.stringify({ noteId: noteId }),
   }).then((_res) => {
+    // var fm = document.referrer;
+    // window.alert(fm);
+    // var wr = fm.charAt(fm.length - 1);
+    // if (wr == "/") {
     window.location.href = "/";
+    // } else {
+    //   window.location.href = "/profile/" + wr;
+    // }
   });
 }
-
