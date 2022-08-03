ad2eb562144525b30b8ab806cd9876d88c892f3f
yejinminghaoshuai <56664524+yejinminghaoshuai@users.noreply.github.com>
2022-08-03 18:50:30 +0800

    bootstrap-based front-end upload
    更新了temp_prof.html作为备选profile样式并为其做了views.py中的适配，暂时没有做超链接路径的修改。直接修改了edit.html, change_password.html

---
+++ flask_web/website/forms.py
@@ -0,0 +1,18 @@
+from wtforms.validators import Email
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+class editEmail(FlaskForm):
+    old_email="123"
+    email = StringField('Email Address', validators=[DataRequired(), Length(1,254)])
+    submit1 = SubmitField('Edit Your Email')
+
+class editNickname(FlaskForm):
+    nickname = StringField('nickname', validators=[DataRequired(), Length(1,20)])
+    submit2 = SubmitField('Edit your nickname')
+
+class changePassword(FlaskForm):
+    password1 = PasswordField('New password',validators=[DataRequired(),Length(8,128)])
+    password2 = PasswordField('Confirm new password',validators=[DataRequired(),Length(8,128)])    
+    submit3 = SubmitField('Confirm')
