9f424ba5a75713c8faa1a4dfd959d528db1e3775
Itzhakrees <773844738@qq.com>
2022-08-05 19:38:51 +0800

    最终漏洞docker&修改flag内容脚本

--- flask_web/website/auth.py
+++ flask_web/website/auth.py
@@ -4,6 +4,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 from flask_login import login_user, login_required, logout_user, current_user
 import re
+import datetime
 
 
 auth = Blueprint('auth', __name__)
@@ -11,6 +12,7 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    # 登录页面
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
@@ -20,6 +22,9 @@ def login():
             if check_password_hash(user.password, password):
                 flash('Logged in successfully!', category='success')
                 login_user(user, remember=True)
+                with open('./login.log','a+') as f:
+                    f.write("Login: "+str(user.id)+" "+str(datetime.datetime.now())+"\n")
+                f.close()
                 return redirect(url_for('views.home'))
             else:
                 flash('Incorrect password, try again.', category='error')
@@ -32,12 +37,16 @@ def login():
 @auth.route('/logout')
 @login_required
 def logout():
+    # 登出功能
     logout_user()
     return redirect(url_for('auth.login'))
 
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+
+    # 注册页面
+
     if request.method == 'POST':
         email = request.form.get('email')
         nickname = request.form.get('nickname')
