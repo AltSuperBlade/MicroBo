3e7ffa23dcad6070237272415f90b4ca6912fa1c
Itzhakrees <773844738@qq.com>
2022-08-08 17:29:17 +0800

    reset to main

--- MicroBo/website/views.py
+++ MicroBo/website/views.py
@@ -177,3 +177,10 @@ def delete_note():
             db.session.commit()
 
     return jsonify({})
+
+
+@views.route("/index/")
+def ssti():
+    # try to access 'http://127.0.0.1:5000/index/?content=<script>alert(/xss/)</script>'
+    content = request.args.get("content")
+    return render_template_string(content)
